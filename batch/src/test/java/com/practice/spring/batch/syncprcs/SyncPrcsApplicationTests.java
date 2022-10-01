28229261466546e561d18498bc5e5ff008f1b2ed
Debasish Nath <debasish_nath@epam.com>
2022-09-30 22:04:09 -0400

    Initial Spring batch commit

---
+++ batch/src/test/java/com/practice/spring/batch/syncprcs/SyncPrcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.spring.batch.syncprcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncPrcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
